94d73e48d4fe6e7168e7beeb8372d432a39f1cd3
mz <mzceltics@163.com>
2020-12-05 13:12:27 +0800

    t2

--- leetcode/chain/t2_add_two_chain.py
+++ leetcode/chain/t2_add_two_chain.py
@@ -1,14 +1,21 @@
 from list_node import ListNode
-def addTwoNumbers(self, l1, l2):
-    node = head = ListNode()
+def addTwoNumbers(l1, l2):
+    head = ListNode(None)
+    tail = head  # tail和head头结点，tail用来游走遍历，head返回最终结果
     tmp = 0
-    node.val = 0
-    return l1 if l1.val == 0 else l2
-    while l1 or l2 or s:
-        node.val = (l1.val + l2.val + tmp) / 10
-        tmp = (l1.val + l2.val) % 10
-        node = node.next
+    while l1 or l2 or tmp != 0:  # tmp != 0, 原因，可能要使得最后一位加一
+        n1 = l1.val if l1 is not None else 0
+        n2 = l2.val if l2 is not None else 0
+        sum_v = n1 + n2 + tmp
+        tail.next = ListNode((sum_v) % 10)
+        tmp = (sum_v) // 10
+        tail = tail.next
         l1 = l1.next if l1 else None
         l2 = l2.next if l2 else None
-    return node
+    return head.next
+
+class ListNode:
+    def __init__(self, val, next):
+        self.val = val
+        self.next = next
 
