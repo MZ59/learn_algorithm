002d87dffd27a53a696ebb7f76e797d5d369bc82
maze <maze@xiaohongshu.com>
2024-06-04 11:25:05 +0800

    update math

--- leetcode/t878.py
+++ leetcode/t878.py
@@ -2,11 +2,12 @@
     y = x//a + x//b - x//lcm(a,b)
     小于等于x的数中能被a或b整除的个数 = 小于等于x的数中能被a整除的个数 + 小于等于x的数中能被a整除的个数 - 小于等于x的数中能被a和b整除的个数
 '''
+import math
 
 def solution(n, a, b):
     left = min(a, b)
     right = n * min(a, b)
-    c = lcm(a, b)  # 最小公倍数
+    c = math.lcm(a, b)  # 最小公倍数
     while left <= right:
         mid = (left + right) // 2
         cnt = mid // a + mid // b - mid // c
